a952089e8652dec043c9c166c1af57e95bbec11b
xuyifei <1227162950@qq.com>
2023-11-05 15:09:22 +0800

    添加部分样式

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 https://nextjs.org/learn/dashboard-app/css-styling
 
 ## 数据库存储
+
 https://vercel.com/xuyifei-youdinet/next-learn/stores/postgres/store_62fxNBpcFCgdwA4V/data
+
+## 模板使用
+
+https://tailwindui.com/components
+
+## unocss
+
+https://blog.csdn.net/halo1416/article/details/131162456
+"editor.quickSuggestions": {
+"strings": true,
+"other": true,
+"comments": true,
+},
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,26 +1,166 @@
 "use client";
-import Image from "next/image";
-import { useState } from "react";
-export default function Page() {
-  const [imageError, setImageError] = useState(false);
-  const errorUrl =
-    "http://47.109.56.148:8080/profile/tmp/upload/13013e9b4e42c3ed3a6ed314a9fab7e_d9cbff184bb543f19fc8859b71f24120.png";
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+const people = [
+  {
+    id: 1,
+    name: "Wade Cooper",
+    avatar:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 2,
+    name: "Arlene Mccoy",
+    avatar:
+      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 3,
+    name: "Devon Webb",
+    avatar:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
+  },
+  {
+    id: 4,
+    name: "Tom Cook",
+    avatar:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 5,
+    name: "Tanya Fox",
+    avatar:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 6,
+    name: "Hellen Schmidt",
+    avatar:
+      "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 7,
+    name: "Caroline Schultz",
+    avatar:
+      "https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 8,
+    name: "Mason Heaney",
+    avatar:
+      "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 9,
+    name: "Claudie Smitham",
+    avatar:
+      "https://images.unsplash.com/photo-1584486520270-19eca1efcce5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 10,
+    name: "Emil Schaefer",
+    avatar:
+      "https://images.unsplash.com/photo-1561505457-3bcad021f8ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+];
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example() {
+  const [selected, setSelected] = useState(people[3]);
+
   return (
-    <div>
-      {imageError ? (
-        <Image alt="" width={200} height={200} src={errorUrl}></Image>
-      ) : (
-        <Image
-          alt=""
-          width={200}
-          height={200}
-          onError={() => {
-            // 图像加载失败时触发的处理函数
-            setImageError(true);
-          }}
-          src="http://47.109.56.148:8080/profile/tmp/upload/JOT-10475%E7%B4%AB%3E8%8F%80%E9%85%AE10376-48-4_0573aa01721b4c8eae7dd68083454da8.jpg"
-        ></Image>
-      )}
-    </div>
+    <>
+      <div className="en:text-orange-400">fasdfas</div>
+
+      <Listbox value={selected} onChange={setSelected}>
+        {({ open }) => (
+          <>
+            <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
+              Assigned to
+            </Listbox.Label>
+            <div className="relative mt-2">
+              <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                <span className="flex items-center">
+                  <img
+                    src={selected.avatar}
+                    alt=""
+                    className="h-5 w-5 flex-shrink-0 rounded-full"
+                  />
+                  <span className="ml-3 block truncate">{selected.name}</span>
+                </span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                  <ChevronUpDownIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
+
+              <Transition
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {people.map((person) => (
+                    <Listbox.Option
+                      key={person.id}
+                      className={({ active }) =>
+                        classNames(
+                          active ? "bg-indigo-600 text-white" : "text-gray-900",
+                          "relative cursor-default select-none py-2 pl-3 pr-9"
+                        )
+                      }
+                      value={person}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <div className="flex items-center">
+                            <img
+                              src={person.avatar}
+                              alt=""
+                              className="h-5 w-5 flex-shrink-0 rounded-full"
+                            />
+                            <span
+                              className={classNames(
+                                selected ? "font-semibold" : "font-normal",
+                                "ml-3 block truncate"
+                              )}
+                            >
+                              {person.name}
+                            </span>
+                          </div>
+
+                          {selected ? (
+                            <span
+                              className={classNames(
+                                active ? "text-white" : "text-indigo-600",
+                                "absolute inset-y-0 right-0 flex items-center pr-4"
+                              )}
+                            >
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </>
+        )}
+      </Listbox>
+    </>
   );
 }
